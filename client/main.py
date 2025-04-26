33f693a31cce3704a6b4d490a74cf5f518d6cb3d
KuKeGeeK <125069493+Kukegeek@users.noreply.github.com>
2025-04-26 22:57:26 +0200

    Update main.py
    routing + lista

--- client/main.py
+++ client/main.py
@@ -1,18 +1,17 @@
-""" Main entry point for Simple Echo Server Client application.
-Client attempts to connect to server on IP Address and Port. 
-Default values are set to 127.0.0.1 (localhost) and 5500.
-Server must be running and listening on that IP and Port before 
-the client can connect. Client program runs until either user
-enters 'quit' or 'shutdown server'.
-"""
 from client import Client
 
 def main():
     c1 = Client("127.0.0.1", 5500)
     message = ""
-    while (message != 'quit') and (message != 'shutdown server'):
-        message = input("Enter message to server: ")
-        c1.send(message)
+    while True:
+        line = input("Destino (puerto) | lista | quit: ")
+        if line == 'quit':
+            break
+        if line == 'lista':
+            c1.send('lista')
+            continue
+        puerto, msg = line.split(' ', 1)
+        c1.send(f"{puerto} {msg}")
 
 if __name__ == '__main__':
     main()
